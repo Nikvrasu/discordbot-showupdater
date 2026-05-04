3c6b64cdf64f7211a4fe5a4c3aecde1eb5170c34
Nikvrasu <nickscodingthing@gmail.com>
2026-05-04 16:09:10 +0200

    fastapi setup + frontpage

--- api/main.py
+++ api/main.py
@@ -1,12 +1,7 @@
-from bot.notifier import Notifier
-from platforms.youtube import Youtube
-from platforms.tmdb import Tmdb
-from config.config import YOUTUBE_API_KEY, DISCORD_API_KEY, TMDB_READ_ACCESS_KEY, TMDB_API_KEY, DISCORD_CHANNEL_ID
-from tasks.scheduler import Scheduler
-from bot.bot import Bot
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    keys = {"YOUTUBE_API_KEY": YOUTUBE_API_KEY, "DISCORD_API_KEY": DISCORD_API_KEY, "TMDB_READ_ACCESS_KEY": TMDB_READ_ACCESS_KEY, "TMDB_API_KEY": TMDB_API_KEY, "DISCORD_CHANNEL_ID": DISCORD_CHANNEL_ID}
-    notifier = Notifier(keys)
-    bot = Bot(notifier=notifier)
-    bot.run(DISCORD_API_KEY)
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "API is running"}
